8def519cb756a4f4e55b07621b4e881850dcf47b
jiho Yoo <yoojihoyoojam@gmail.com>
2026-07-23 15:15:15 +0900

    Create buildings.py

---
+++ city_builder/buildings.py
@@ -0,0 +1,35 @@
+BUILDINGS = {
+
+    "🏠": {
+        "name": "주택",
+        "cost": 100,
+        "income": 5,
+        "population": 5,
+        "happiness": 1
+    },
+
+    "🏪": {
+        "name": "상점",
+        "cost": 250,
+        "income": 30,
+        "population": 0,
+        "happiness": 0
+    },
+
+    "🏭": {
+        "name": "공장",
+        "cost": 400,
+        "income": 80,
+        "population": 0,
+        "happiness": -3
+    },
+
+    "🌳": {
+        "name": "공원",
+        "cost": 200,
+        "income": 0,
+        "population": 0,
+        "happiness": 8
+    }
+
+}
